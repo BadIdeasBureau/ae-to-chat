639754c26a35821d16bc00006b559254c4931f04
BadIdeasBureau <dumbmuscle@gmail.com>
2021-01-27 23:34:37 +0000

    Refactor anchors + name effects in confirmation

--- scripts/ae-to-chat.js
+++ scripts/ae-to-chat.js
@@ -70,100 +70,45 @@ async function _onRenderChatMessage(app, html, data) {
 		showInfoEffectAnchor?.parent().hide();
 	}
 
-//TODO later:  Refactor these anchors to deduplicate code further?  Could probably do a switch for the "do the thing and generate content" section
+
 	deleteEffectAnchor?.on("click", event => {
-		const anchorHandler = _anchorHandler(event);
-		if (!anchorHandler) return;
-		const tokenData = anchorHandler.tokenData;
-		const speaker = anchorHandler.speaker;
-		const effectId = anchorHandler.effectId;
+		async function anchorDelete(token, effectId, effectName){
+			let content = ""                                    
+			if(! await token.actor.effects.get(effectId)?.delete()){
+				content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
+			} else {content = `${game.i18n.localize("AE_TO_CHAT.ChatCard.EffectDeleted")} ${effectName}`};
+			return content
+		}
 
-		//Make a voodoo doll of the token from the incomplete token data
-		const token = new Token(tokenData);
-
-		//Use the voodoo doll to delete the effect from the actual token (if it exists).  It is still kinda nonsense that this works, but let's roll with it
-		let content = ""                                    
-		if(!token.actor.effects.get(effectId)?.delete()){
-			content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
-		} else {content = `${game.i18n.localize("AE_TO_CHAT.ChatCard.EffectDeleted")} ${effectId}`};
-
-		//Confirm in chat
-
-		const chatUser = game.userId;
-		const chatType = CONST.CHAT_MESSAGE_TYPES.WHISPER;
-		
-		//make the chat message, and whisper to the person that pressed the button.
-		return ChatMessage.create({
-			content,
-			type: chatType,
-			user: chatUser,
-			whisper: [chatUser]
-		});
+		_anchorHandler(event, anchorDelete);
 	});
 
 	disableEffectAnchor?.on("click", event => {
-		const anchorHandler = _anchorHandler(event);
-		if (!anchorHandler) return;
-		const tokenData = anchorHandler.tokenData;
-		const speaker = anchorHandler.speaker;
-		const effectId = anchorHandler.effectId;
-
-		//Make a voodoo doll of the token from the incomplete token data
-		const token = new Token(tokenData);
-
-		//Use the voodoo doll to disable the effect from the actual token (if it exists).  It is still kinda nonsense that this works, but let's roll with it
-		let content = ""                                    
-		if(!token.actor.effects.get(effectId)?.update({disabled: true})){
-			content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
-		} else {content = `${game.i18n.localize("AE_TO_CHAT.ChatCard.EffectDisabled")} ${effectId}`};
-
-		//Confirm in chat
-
-		const chatUser = game.userId;
-		const chatType = CONST.CHAT_MESSAGE_TYPES.WHISPER;
+		async function anchorDisable(token, effectId, effectName) {
+			let content;                                    
+			if(! await token.actor.effects.get(effectId)?.update({disabled: true})){
+				content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
+			} else {content = `${game.i18n.localize("AE_TO_CHAT.ChatCard.EffectDisabled")} ${effectName}`};
+			return content;
+		}
 		
-		//make the chat message, and whisper to the person that pressed the button.
-		return ChatMessage.create({
-			content,
-			type: chatType,
-			user: chatUser,
-			whisper: [chatUser]
-		});
+		_anchorHandler(event, anchorDisable);
 	});
 	
 	showInfoEffectAnchor?.on("click", event => {
-		const anchorHandler = _anchorHandler(event);
-		if (!anchorHandler) return;
-		const tokenData = anchorHandler.tokenData;
-		const speaker = anchorHandler.speaker;
-		const effectId = anchorHandler.effectId;
-
-		//Make a voodoo doll of the token from the incomplete token data
-		const token = new Token(tokenData);
-
-		//Use the voodoo doll to show the effect from the actual token (if it exists).  It is still kinda nonsense that this works, but let's roll with it
-		let content = ""                                    
-		if(!token.actor.effects.get(effectId)?.sheet.render(true)){
-			content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
-			//no chat annoucement if successful - the sheet will pop up, which should be obvious
-
-			//Confirm in chat
-
-			const chatUser = game.userId;
-			const chatType = CONST.CHAT_MESSAGE_TYPES.WHISPER;
-		
-			//make the chat message, and whisper to the person that pressed the button.
-			return ChatMessage.create({
-				content,
-				type: chatType,
-				user: chatUser,
-				whisper: [chatUser]
-			});
+		async function anchorShowInfo(token, effectId, effectName) {
+			let content;                                    
+			if(! await token.actor.effects.get(effectId)?.sheet.render(true)){
+				content = game.i18n.localize("AE_TO_CHAT.ChatCard.NoEffect")
+			}
+			return content;
 		}
+		
+		_anchorHandler(event, anchorShowInfo);
 	});
 }
 
-function _anchorHandler(event){
+async function _anchorHandler(event, anchorAction){
 	//takes an event, returns null if either message, speaker, or token are missing, and returns {speaker, tokenData, effectId} otherwise
 	//Speaker may not be needed in there, but it's a handy thing that contains a ton of other data, so... might as well
 	//Grab all the relevant data from the message
@@ -173,14 +118,34 @@ function _anchorHandler(event){
 	const messageId = messageListItem?.dataset?.messageId;
 	const message = messageId ? game.messages.get(messageId) : null;
 	//Check the message still exists (and wonder how they managed to click the button if it didn't)
-	if (!message) return null;
+	if (!message) return;
 	const speaker = message?.data?.speaker;
 	//Check the message has a speaker
-	if (!speaker) return null;
+	if (!speaker) return;
 	const tokenData = game.scenes.get(speaker.scene).data.tokens.find(t => t._id===speaker.token);
 
 	//Make sure the tokenData was fetched (i.e. the token exists)
-	if (!tokenData) return null;
-	//output speaker and tokenData
-	return {speaker: speaker, tokenData: tokenData, effectId: effectId};
+	if (!tokenData) return;
+
+	//Make a voodoo doll of the token from the incomplete token data
+	const token = new Token(tokenData);
+	const effectName = token.actor.effects.get(effectId)?.data.label;
+
+	//pass the voodoo doll to the function that does the thing
+	let content = await anchorAction(token, effectId, effectName);
+	
+	//Confirm in chat
+	
+	const chatUser = game.userId;
+	const chatType = CONST.CHAT_MESSAGE_TYPES.WHISPER;
+			
+	//make the chat message, and whisper to the person that pressed the button.
+	if(content){
+		return ChatMessage.create({
+			content,
+			type: chatType,
+			user: chatUser,
+			whisper: [chatUser]
+		});
+	}
 }
